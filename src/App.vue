8a3602be4374345fff658d835fe35def835b7218
Elliot Forbes <emforce@live.com>
2019-05-18 21:58:04 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <simple-component/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SimpleComponent from './components/SimpleComponent.vue';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    SimpleComponent
   }
 }
 </script>
